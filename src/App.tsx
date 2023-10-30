a1852e46fcbef7eb67c08efc2c2d6955bbd48efb
Derek <109226053+derekjtong@users.noreply.github.com>
2023-10-30 01:08:10 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Select, { SelectOption } from "./Select";
+
+const options = [
+  {
+    label: "First",
+    value: 1,
+  },
+  {
+    label: "Second",
+    value: 2,
+  },
+  {
+    label: "Third",
+    value: 3,
+  },
+  {
+    label: "Fourth",
+    value: 4,
+  },
+];
+
+function App() {
+  const [value, setValue] = useState<SelectOption | undefined>(options[0]);
+  return (
+    <h1>
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
+    </h1>
+  );
+}
+
+export default App;
