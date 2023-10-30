8b7311e1de11d4211ec62034660b2c20856990b5
Derek <109226053+derekjtong@users.noreply.github.com>
2023-10-30 01:50:05 -0400

    Added support for multiple options

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,16 @@ const options = [
 
 function App() {
   const [value, setValue] = useState<SelectOption | undefined>(options[0]);
+  const [value2, setValue2] = useState<SelectOption[]>([options[0]]);
   return (
     <h1>
       <Select options={options} value={value} onChange={(o) => setValue(o)} />
+      <Select
+        multiple
+        options={options}
+        value={value2}
+        onChange={(o) => setValue2(o)}
+      />
     </h1>
   );
 }
